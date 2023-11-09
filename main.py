f71c43897627ae77bafaf826ea0bf4eae4fbf485
AngelicaBGL <angelicaluciano@alunos.utfpr.edu.br>
2023-11-08 21:59:15 -0300

    main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-aaaaaaaaaaaaa
+from Game import *
+import sys
+
+
+
+if __name__== "__main__":
+    if len(sys.argv) !=5:
+        print("Uso: python3 programa.py entrada1.txt op.txt temp.txt saida.txt")
+        sys.exit(1)
